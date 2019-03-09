440f31ae028c55d3ca61dd45867416e868862e0c
David <david@MacBook-Pro-2.local>
2019-03-09 16:48:28 -0500

    added card objects

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,18 +1,40 @@
-var cards = ["queen", "queen", "king", "king"]
+var cards = [
+    {
+        rank: "queen",
+        suit: "hearts",
+        cardImage: "images/queen-of-hearts.png"
+    },
+    {
+        rank: "queen",
+        suit: "diamonds",
+        cardImage: "images/queen-of-diamonds.png"
+    },
+    {
+        rank: "king",
+        suit: "hearts",
+        cardImage: "images/king-of-hearts.png"
+    },
+    {
+        rank: "king",
+        suit: "diamonds",
+        cardImage: "images/queen-of-diamonds.png"
+    }
+]
 var cardsInPlay = []
 
-var checkForMatch = function() {
+var checkForMatch = function () {
     if (cardsInPlay[0] === cardsInPlay[1]) {
-        console.log('You found a match!')
+        alert("You found a match!")
     } else {
-        console.log('Sorry, try again.')
+        alert("Sorry, try again.")
     }
 }
 
 var flipCard = function (cardId) {
-    checkForMatch()
-    cardsInPlay.push(cards[cardId])
-    console.log("User flipped " + cards[cardId])
+    checkForMatch();
+    cardsInPlay.push(cards[cardId].rank);
+    console.log("User flipped " + cards[cardId].rank);
+    console.log("User flipped " + cards[cardId].cardImage)
 }
 flipCard(0);
 flipCard(2);
